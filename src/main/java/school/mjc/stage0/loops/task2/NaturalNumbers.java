43aa4d78c63860e7d7291b0f90cee1ed43a5ac5f
TypicalDad <119784728+TypicalDad@users.noreply.github.com>
2022-12-17 08:54:38 +0300

    Update NaturalNumbers.java

--- src/main/java/school/mjc/stage0/loops/task2/NaturalNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/NaturalNumbers.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.loops.task2;
 
 public class NaturalNumbers {
     public void naturalNumbersPrinter(int lastPrinted) {
+        int zero = -1;
+        while (zero!=lastPrinted){
+            zero += 1;
+            System.out.println(zero);
+        }
     }
 }
