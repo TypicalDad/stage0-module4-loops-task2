5fc23c7be57ce456c888c70e6f85cb8781267dd9
TypicalDad <119784728+TypicalDad@users.noreply.github.com>
2022-12-17 11:28:44 +0300

    Update EvenNumbersPrinter.java

--- src/main/java/school/mjc/stage0/loops/task2/EvenNumbersPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task2/EvenNumbersPrinter.java
@@ -2,10 +2,10 @@ package school.mjc.stage0.loops.task2;
 
 public class EvenNumbersPrinter {
     public void printEvenNumbers(int printTillInclusive) {
-        int zero = -2;
-        while (zero!=printTillInclusive){
-            zero += 2;
-            System.out.println(zero);
-        }
+            int zero = 0;
+            while (zero<printTillInclusive){
+                System.out.println(zero);
+                zero += 2;
+            }
     }
 }
