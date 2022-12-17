0bf370aef7b1d5e29c8b1a253488e634f144c33a
TypicalDad <119784728+TypicalDad@users.noreply.github.com>
2022-12-17 09:26:35 +0300

    Update PrimeNumbers.java

--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,5 +2,26 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
+        for (int i=2;i<printToInclusive;i++){
+            if(checkSimple(i))
+                System.out.println(i);
+        }
+    
+
+    public static boolean checkSimple(int i){
+        if (i<=1)
+            return false;
+        else if (i <=3)
+            return true;
+        else if (i%2==0 || i %3 ==0)
+            return false;
+        int n = 5;
+        while (n*n <=i){
+            if (i % n ==0 || i % (n+2) == 0)
+                return false;
+            n=n+6;
+        }
+        return true;
+    }
     }
 }
